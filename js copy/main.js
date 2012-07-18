7ff57a422b4b2bad4fdaf49b96e70956472839d8
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-07-17 22:46:06 -0400

    finished preparing my add edit and delete links.

--- js copy/main.js
+++ js copy/main.js
@@ -120,6 +120,7 @@ function getData() {
 	$("items").style.display="block";
 	for(var i=0, len=localStorage.length; i<len; i++){
 		var makeLi =document.createElement("li");
+		var linksLi = document.createElement("li"); //WEEK 3 ADD
 		makeList.appendChild(makeLi);
 		var key = localStorage.key(i);
 		var value = localStorage.getItem(key);
@@ -132,8 +133,10 @@ function getData() {
 			makeSubList.appendChild(makeSubLi);
 			var optSubText = obj [n] [0] + " "+obj[n][1];
 			makeSubLi.innerHTML = optSubText;
+			makeSubList.appendChild(linksLi);
 
 		}
+		makeItemsLink();//create our edit and delete buttons/link fr each item in local storage//WEEK 3 ADD
 	}
 }
 
